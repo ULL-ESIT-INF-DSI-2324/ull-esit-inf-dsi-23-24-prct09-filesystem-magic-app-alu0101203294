85702874983f6169f6fb789911205bd70e28f0c2
alu0101203294 <alu0101203294@ull.edu.es>
2024-03-20 11:06:00 +0000

    TypeDoc Modificacion

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJKTElR0lEqSCzJULJSSivNSy7JzM8r1k9MSdHLKMnNUdJRys7MS1GyMjOpjQUAnSvCmTYAAAA="
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJyDg4LKMpPTi1OTMkvUtJRKkgsyVCyUkrOSSwuTi3WR5HWyyjJzVHSUcrOzEtRsjI0sqjVgZvjFezvh88gVHlMk2IBeYbbb5MAAAA="
